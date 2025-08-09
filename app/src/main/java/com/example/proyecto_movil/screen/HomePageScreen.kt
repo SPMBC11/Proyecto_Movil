3c757c71caee394ff035a1b20fb641602fada01e
JuanV3rgas <juan.variza@gmail.com>
2025-08-09 18:31:34 -0500

    Desarrollo de la pantalla HomePageScreen.kt

--- app/src/main/java/com/example/proyecto_movil/screen/HomePageScreen.kt
+++ app/src/main/java/com/example/proyecto_movil/screen/HomePageScreen.kt
@@ -1,13 +1,58 @@
 package com.example.proyecto_movil.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun HomePageScreen(
+
     onBack: () -> Unit = {},
-    onLogin: (email: String, password: String, remember: Boolean) -> Unit = { _, _, _ -> },
-    onForgotPassword: () -> Unit = {},
-    onRegister: () -> Unit = {},
-    onGoogle: () -> Unit = {},
-    onFacebook: () -> Unit = {},
-    onApple: () -> Unit = {},
+){
+
+
+
+    val bg = Color(0xFF0E0E0E)
+    val card = Color(0xFF151515)
+    val accent = Color(0xFF20D0C2)
+    val fieldText = Color(0xFFEDEDED)
+    val hint = Color(0xFF9A9A9A)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(bg)
+            .padding(horizontal = 20.dp)
+    ){
+        IconButton(
+            onClick = onBack,
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(top = 8.dp)
+        ) {
+            Icon(Icons.Default.Logout, contentDescription = "Atrás", tint = Color.White)
+        }
+
+
+    }
+    Text("Bienvenido de vuelta Juan")
+}
+
+@Composable
+@Preview(showBackground = true)
+fun HomePageScreenPreview(){
+    HomePageScreen()
+}

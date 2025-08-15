96c20725f7b90f319e52d9b1a2f4fa0ec271c9a0
Santiago Pineda Mora <santiagopm1204@gmail.com>
2025-08-15 11:06:00 -0500

    Correccion de HomePageScreen.kt y RegisterScreen2.kt

---
+++ app/src/main/java/com/example/proyecto_movil/utils/Componentes.kt
@@ -0,0 +1,115 @@
+package com.example.proyecto_movil.ui.utils
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.proyecto_movil.R
+
+@Composable
+fun LogoApp(
+    modifier: Modifier = Modifier
+){
+    Image(
+        painter = painterResource(R.drawable.logo),
+        contentDescription = ("Logo critichord"),
+        modifier = Modifier.size(120.dp)
+    )
+}
+
+@Composable
+fun AppButton(
+    Textoboton: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = colorResource(R.color.teal_200),
+            contentColor = colorResource(R.color.white)
+        ),
+        modifier = modifier
+
+    )
+    {
+        Text(Textoboton)
+    }
+}
+@Composable
+fun Registrate(
+    texto : String,
+    modifier: Modifier = Modifier
+){
+    Text(
+        text = texto,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold,
+        color = colorResource(R.color.white),
+        modifier = modifier
+    )
+}
+@Composable
+fun Terminos(
+    texto : String,
+    modifier: Modifier = Modifier
+){
+    Text(
+        text = texto,
+        fontSize = 15.sp,
+        fontWeight = FontWeight.Light,
+        color = colorResource(R.color.white),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun YatienesCuenta(texto : String,
+                   modifier: Modifier = Modifier
+){
+    Text(
+        text = texto,
+        fontSize = 15.sp,
+        fontWeight = FontWeight.Light,
+        color = colorResource(R.color.white),
+        modifier = modifier
+    )
+}
+@Composable
+fun CheckboxDatos(modifier: Modifier = Modifier) {
+    var checked by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { checked = it },
+            colors = CheckboxDefaults.colors(
+                checkedColor = colorResource(R.color.teal_200),
+                uncheckedColor = colorResource(R.color.white),
+                checkmarkColor = colorResource(R.color.white),
+
+                )
+        )
+    }
+
+}
+

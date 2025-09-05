b39d4d21ab6261a6076bb44d8d69d0ac25823b3a
Santiago Jose hernandez <www.santiagohernandez@gmail.com>
2025-09-04 21:22:21 -0500

    nao peudo adelantar mas

--- app/src/main/java/com/example/proyecto_movil/screen/HomePage/HomeViewModel.kt
+++ app/src/main/java/com/example/proyecto_movil/screen/HomePage/HomeViewModel.kt
@@ -2,11 +2,16 @@ package com.example.proyecto_movil.screen.HomePage
 
 import androidx.lifecycle.ViewModel
 import com.example.proyecto_movil.data.local.AlbumRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class HomeViewModel: ViewModel() {
-
+data class Objeto(
+val nombre: String)
+@HiltViewModel
+class HomeViewModel(): ViewModel() {
     private val _state = MutableStateFlow(HomeState())
     val state: StateFlow<HomeState> = _state.asStateFlow()
 

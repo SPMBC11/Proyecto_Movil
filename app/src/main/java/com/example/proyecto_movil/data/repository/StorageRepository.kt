79c0f96c30a91d87c02e4a401f600b4cc54e9559
Santiago Jose hernandez <www.santiagohernandez@gmail.com>
2025-09-25 18:03:41 -0500

    Updated

--- app/src/main/java/com/example/proyecto_movil/data/repository/StorageRepository.kt
+++ app/src/main/java/com/example/proyecto_movil/data/repository/StorageRepository.kt
@@ -1,4 +1,27 @@
 package com.example.proyecto_movil.data.repository
 
-class StorageRepository {
+import android.net.Uri
+import com.example.proyecto_movil.data.datasource.AuthRemoteDataSource
+import com.example.proyecto_movil.data.datasource.StorageRemoteDataSource
+import jakarta.inject.Inject
+
+class StorageRepository @Inject constructor(
+    private val storage: StorageRemoteDataSource,
+    private val auth: AuthRemoteDataSource
+) {
+
+    suspend fun uploadProfileImage(uri: Uri): Result<String> {
+        return try {
+            val userId = auth.currentUser?.uid ?: return Result.failure(
+                Exception(
+                    "No se encontro un usuario"
+                )
+            )
+            val path = "ProfileImages/$userId.jpg"
+            val url = storage.uploadImage(path, uri)
+            Result.success(url)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
